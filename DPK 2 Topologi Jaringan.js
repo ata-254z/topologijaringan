6cceda93cc61e770854d60838853654077ef41f8
Ata <ririn110283@gmail.com>
2024-11-23 14:06:19 +0700

    Delete DPK 2 Topologi Jaringan.js

--- DPK 2 Topologi Jaringan.js
+++
@@ -1,2 +0,0 @@
-function canva(){window.location.href="https://www.canva.com/design/DAGXQlvUDkY/CSUYxPc1dDj8h_v3UlL5Kw/view?utm_content=DAGXQlvUDkY&utm_campaign=designshare&utm_medium=link&utm_source=editor";}
-window.onload=canva();
