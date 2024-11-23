523f8c305d1e2cada034f665a5a83b9ef5ada07c
Ata <ririn110283@gmail.com>
2024-11-23 14:17:03 +0700

    Delete DPK2.js

--- DPK2.js
+++
@@ -1,3 +0,0 @@
-window.onload=function(){
-    window.location.href="https://www.canva.com/design/DAGXQlvUDkY/CSUYxPc1dDj8h_v3UlL5Kw/view?utm_content=DAGXQlvUDkY&utm_campaign=designshare&utm_medium=link&utm_source=editor";
-};
