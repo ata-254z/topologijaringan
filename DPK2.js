c0240b6451cc66421de84cb2de7d821441ad6065
Ata <ririn110283@gmail.com>
2024-11-23 14:09:11 +0700

    Add files via upload

--- DPK2.js
+++ DPK2.js
@@ -1,2 +1,3 @@
-function canva(){window.location.href="https://www.canva.com/design/DAGXQlvUDkY/CSUYxPc1dDj8h_v3UlL5Kw/view?utm_content=DAGXQlvUDkY&utm_campaign=designshare&utm_medium=link&utm_source=editor";}
-window.onload=canva();
+window.onload=function(){
+    window.location.href="https://www.canva.com/design/DAGXQlvUDkY/CSUYxPc1dDj8h_v3UlL5Kw/view?utm_content=DAGXQlvUDkY&utm_campaign=designshare&utm_medium=link&utm_source=editor";
+};
