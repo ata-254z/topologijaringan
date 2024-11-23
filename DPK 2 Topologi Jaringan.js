96209fa9a567cea85f9d9ac249169485d8420683
Ata <ririn110283@gmail.com>
2024-11-23 14:00:47 +0700

    Add files via upload

---
+++ DPK 2 Topologi Jaringan.js
@@ -0,0 +1,2 @@
+function canva(){window.location.href="https://www.canva.com/design/DAGXQlvUDkY/CSUYxPc1dDj8h_v3UlL5Kw/view?utm_content=DAGXQlvUDkY&utm_campaign=designshare&utm_medium=link&utm_source=editor";}
+window.onload=canva();
